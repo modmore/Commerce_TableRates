a67c26bf854d293eb5150b912d02a111f434f0c0
Mark Hamstra <hello@markhamstra.com>
2018-01-04 00:03:54 +0100

    Fix tests related to changes in the step requirements

--- core/components/commerce_tablerates/lexicon/nl/default.inc.php
+++ core/components/commerce_tablerates/lexicon/nl/default.inc.php
@@ -1,15 +1,15 @@
 <?php
 
-$_lang['commerce_tablerates'] = 'TableRates Shipping Method';
-$_lang['commerce_tablerates.module_description'] = 'When the module is enabled, a new TableRates shipping method becomes available in Commerce > Configuration > Shipping Methods. This shipping method supports managing shipping rates with comma separated data that is compatible with Magento\'s TableRates system for managing dynamic shipping rates.';
-$_lang['commerce_tablerates.section_title'] = 'TableRates';
-$_lang['commerce_tablerates.condition'] = 'Condition';
-$_lang['commerce_tablerates.condition.weight'] = 'Weight to destination';
-$_lang['commerce_tablerates.condition.price'] = 'Price to destination';
-$_lang['commerce_tablerates.condition.items'] = 'Number of items to destination';
-$_lang['commerce_tablerates.csvdata'] = 'Comma-separated data';
-$_lang['commerce_tablerates.csvdata.description'] = 'The comma separated data that contains your rules for destinations and the associated condition, and shipping price. Per row, this should contain: country code (2 character prefered, 3 character supported), region/state, zipcode, the value for the selected condition, and the shipping price as decimal.';
+$_lang['commerce_tablerates'] = 'Tarieventabel verzendmethode';
+$_lang['commerce_tablerates.module_description'] = 'Als deze module is ingeschakeld is, zal een nieuwe tarieventabel verzendmethode beschikbaar zijn in Commerce > Configuratie > Verzendmethoden. Deze verzendmethode maakt het mogelijk om verzendtarieven te beheren met een kommagescheiden lijst van bestemmingen, gewicht, aantal producten, of subtotaal. Dit is compatibel met Magento\'s Table Rates systeem voor dynamische verzendtarieven.';
+$_lang['commerce_tablerates.section_title'] = 'Tarieventabel';
+$_lang['commerce_tablerates.condition'] = 'Voorwaarde';
+$_lang['commerce_tablerates.condition.weight'] = 'Gewicht / bestemming';
+$_lang['commerce_tablerates.condition.price'] = 'Prijs / bestemming';
+$_lang['commerce_tablerates.condition.items'] = 'Aantal producten / bestemming';
+$_lang['commerce_tablerates.csvdata'] = 'Kommagescheiden gegevens';
+$_lang['commerce_tablerates.csvdata.description'] = 'De kommagescheiden regels voor bestemmingen en de bijbehorende voorwaarde en verzendprijs. Per rij moet dit het volgende bevatten: landcode (bij voorkeur 2 karakter code, 3 karakter code ook ondersteund), provincie/staat, postcode, de waarde voor de geselecteerde voorwaarde, en de verzendprijs als decimaal nummer.';
 
 // Some lexicons are automatically called by the commerce core, based on the product class name, so add those.
-$_lang['commerce.TableRatesShippingMethod'] = 'TableRates Shipping Method';
-$_lang['commerce.add_TableRatesShippingMethod'] = 'Add TableRates Shipping Method';
+$_lang['commerce.TableRatesShippingMethod'] = 'Tarieventabel verzendmethode';
+$_lang['commerce.add_TableRatesShippingMethod'] = 'Tarieventabel verzendmethode toevoegen';
