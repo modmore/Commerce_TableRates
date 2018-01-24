53fb336370ed7998cd9ff0a6f930eda45b3270ad
Mark Hamstra <hello@markhamstra.com>
2018-01-24 11:45:36 +0100

    Translation sync

--- core/components/commerce_tablerates/lexicon/de/default.inc.php
+++ core/components/commerce_tablerates/lexicon/de/default.inc.php
@@ -1,15 +1,15 @@
 <?php
 
-$_lang['commerce_tablerates'] = 'TableRates Shipping Method';
-$_lang['commerce_tablerates.module_description'] = 'When the module is enabled, a new TableRates shipping method becomes available in Commerce > Configuration > Shipping Methods. This shipping method supports managing shipping rates with comma separated data that is compatible with Magento\'s TableRates system for managing dynamic shipping rates.';
-$_lang['commerce_tablerates.section_title'] = 'TableRates';
-$_lang['commerce_tablerates.condition'] = 'Condition';
-$_lang['commerce_tablerates.condition.weight'] = 'Weight to destination';
-$_lang['commerce_tablerates.condition.price'] = 'Price to destination';
-$_lang['commerce_tablerates.condition.items'] = 'Number of items to destination';
-$_lang['commerce_tablerates.csvdata'] = 'Comma-separated data';
-$_lang['commerce_tablerates.csvdata.description'] = 'The comma separated data that contains your rules for destinations and the associated condition, and shipping price. Per row, this should contain: country code (2 character prefered, 3 character supported), region/state, zipcode, the value for the selected condition, and the shipping price as decimal.';
+$_lang['commerce_tablerates'] = 'Tabellenbasierte Versandmethode';
+$_lang['commerce_tablerates.module_description'] = 'Wenn dieses Modul aktiviert wird, wird eine neue Versandmethode namens "Tabellenbasierte Versandmethode" unter Commerce > Konfiguration > Versandmethoden verfügbar. Diese tabellenbasierte Versandmethode unterstützt die Komma-separierte Versandart, wie sie auch in Magento verwendet wird, um dynamische Versandarten bereitzustellen. Kompatibel mit der tablerates.csv Datei aus Magento.';
+$_lang['commerce_tablerates.section_title'] = 'Tabellenbasiert';
+$_lang['commerce_tablerates.condition'] = 'Bedingung';
+$_lang['commerce_tablerates.condition.weight'] = 'Gewicht gegen Ziel';
+$_lang['commerce_tablerates.condition.price'] = 'Preis gegen Ziel';
+$_lang['commerce_tablerates.condition.items'] = 'Produktanzahl gegen Ziel';
+$_lang['commerce_tablerates.csvdata'] = 'Komma-separierte Daten';
+$_lang['commerce_tablerates.csvdata.description'] = 'Die Komma-separierten Daten, die Ihre Regeln bezüglich Ziel, Bedingung (Gewicht oder Entfernung) und Preis beinhalten. Pro Zeile sollten folgende Angaben gemacht werden: Länderkürzel (2-stellig präferiert, 3-stellig wird auch unterstützt), Region/Bundesland, PLZ, Wert der gewählten Bedingung, und der Preis als Dezimalstelle. Beispiel für Gewicht (in Gramm) gegen Ziel: <code>DE,*,*,300,2.50</code>';
 
 // Some lexicons are automatically called by the commerce core, based on the product class name, so add those.
-$_lang['commerce.TableRatesShippingMethod'] = 'TableRates Shipping Method';
-$_lang['commerce.add_TableRatesShippingMethod'] = 'Add TableRates Shipping Method';
+$_lang['commerce.TableRatesShippingMethod'] = 'Tabellenbasierte Versandmethode';
+$_lang['commerce.add_TableRatesShippingMethod'] = 'Tabellenbasierte Versandmethode hinzufügen';
--- core/components/commerce_tablerates/lexicon/id/default.inc.php
+++ core/components/commerce_tablerates/lexicon/id/default.inc.php
@@ -1,15 +1,16 @@
 <?php
 
-$_lang['commerce_tablerates'] = 'TableRates Shipping Method';
-$_lang['commerce_tablerates.module_description'] = 'When the module is enabled, a new TableRates shipping method becomes available in Commerce > Configuration > Shipping Methods. This shipping method supports managing shipping rates with comma separated data that is compatible with Magento\'s TableRates system for managing dynamic shipping rates.';
-$_lang['commerce_tablerates.section_title'] = 'TableRates';
-$_lang['commerce_tablerates.condition'] = 'Condition';
-$_lang['commerce_tablerates.condition.weight'] = 'Weight to destination';
-$_lang['commerce_tablerates.condition.price'] = 'Price to destination';
-$_lang['commerce_tablerates.condition.items'] = 'Number of items to destination';
-$_lang['commerce_tablerates.csvdata'] = 'Comma-separated data';
-$_lang['commerce_tablerates.csvdata.description'] = 'The comma separated data that contains your rules for destinations and the associated condition, and shipping price. Per row, this should contain: country code (2 character prefered, 3 character supported), region/state, zipcode, the value for the selected condition, and the shipping price as decimal.';
+$_lang['commerce_tablerates'] = 'Tabel Harga Metode Pengiriman';
+$_lang['commerce_tablerates.module_description'] = 'Saat modul diaktifkan, metode pengiriman Tarif Tabel baru tersedia di perdagangan > Konfigurasi > Metode pengiriman. Metode pengiriman ini mendukung pengelolaan tarif pengiriman dengan data yang dipisahkan dengan koma yang sesuai dengan sistem Tabel Magento untuk mengelola tarif pengiriman dinamis.';
+$_lang['commerce_tablerates.section_title'] = 'Tabel harga';
+$_lang['commerce_tablerates.condition'] = 'Kondisi';
+$_lang['commerce_tablerates.condition.weight'] = 'Berat ke tujuan';
+$_lang['commerce_tablerates.condition.price'] = 'Harga ke tujuan';
+$_lang['commerce_tablerates.condition.items'] = 'Jumlah item ke tujuan';
+$_lang['commerce_tablerates.csvdata'] = 'Data yang dipisahkan koma';
+$_lang['commerce_tablerates.csvdata.description'] = 'Data dipisahkan koma yang berisi aturan untuk tujuan dan kondisi yang terkait, dan harga pengiriman. Per baris, ini harus berisi:
+kode negara (2 karakter lebih disukai, didukung 3 karakter), wilayah/negara bagian, kode pos, nilai untuk kondisi yang dipilih, dan harga pengiriman sebagai desimal.';
 
 // Some lexicons are automatically called by the commerce core, based on the product class name, so add those.
-$_lang['commerce.TableRatesShippingMethod'] = 'TableRates Shipping Method';
-$_lang['commerce.add_TableRatesShippingMethod'] = 'Add TableRates Shipping Method';
+$_lang['commerce.TableRatesShippingMethod'] = 'Tabel Harga Metode Pengiriman';
+$_lang['commerce.add_TableRatesShippingMethod'] = 'Tambahkan Tabel Harga Metode Pengiriman';
