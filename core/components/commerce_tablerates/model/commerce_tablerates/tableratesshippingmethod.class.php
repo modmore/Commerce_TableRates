f6d6119785c8a8410491bac6e3decd27e74c79e5
Mark Hamstra <hello@markhamstra.com>
2018-04-06 16:06:43 +0200

    Commerce now automatically selects the first available shipping method, as soon as the cart is shown

--- core/components/commerce_tablerates/model/commerce_tablerates/tableratesshippingmethod.class.php
+++ core/components/commerce_tablerates/model/commerce_tablerates/tableratesshippingmethod.class.php
@@ -328,6 +328,9 @@ class TableRatesShippingMethod extends comShippingMethod
         }
         $address = $order->getShippingAddress();
         if (!$address) {
+            $address = $order->getExpectedAddress();
+        }
+        if (!$address) {
             return parent::getPriceForShipment($shipment);
         }
 
